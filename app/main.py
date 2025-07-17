db16baec1e2bb4b3dadc86e843b4023091cf7da5
ymushka <ymushka@waverleysoftware.com>
2025-07-18 00:33:48 +0200

    Solution3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Animal:
-    def __init__(self, name: str, appetite: int, is_hungry=True) -> None:
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -17,7 +17,7 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name: str, appetite=3, is_hungry=True) -> None:
+    def __init__(self, name: str, appetite: int = 3, is_hungry: bool = True) -> None:
         super().__init__(name, appetite, is_hungry)
 
     @staticmethod
@@ -26,7 +26,7 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(self, name: str, appetite=7, is_hungry=True) -> None:
+    def __init__(self, name: str, appetite: int = 7, is_hungry: bool = True) -> None:
         super().__init__(name, appetite, is_hungry)
 
     @staticmethod
